3803ec7e229fc8c708b3499f22f0f32e9cf3a955
Bill Li <me@billxinli.com>
2013-07-17 23:53:24 -0400

    Adding authentication with staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_staging
+  protected
+  def authenticate_staging
+    if Rails.env.staging?
+      authenticate_or_request_with_http_basic 'Staging' do |name, password|
+        name == 'username' && password == 'password'
+      end
+    end
+  end
 end
