f93efd5444583de61940ad37240e61aec74da8d3
gommy <gommy.y@hotmail.com>
2013-10-11 23:31:05 -0700

    load course from db on lanidng page

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,5 +1,6 @@
 class LandingsController < ApplicationController
   def index
-    redirect_to user_path current_user and return if current_user
+    redirect_to user_path(current_user) and return if current_user
+    @courses = Course.includes(:owner).limit(12)
   end
 end
