b4328b495e0aec65e4442f9088fc4fda2cdc3c11
gommy <gommy.y@hotmail.com>
2013-10-12 01:43:32 -0700

    change landing page(me)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def profile
     redirect_to user_welcome_path if current_user.sign_in_count == 1
-    @tab = params[:tab] || 'goal'
+    #@tab = params[:tab] || 'goal'
     if @owner
       @goal_users = GoalUser.goal_for(@user).active.includes(:goal, :checkins).all
       @checkins = Checkin.for_user(@user).all
