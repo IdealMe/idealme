7ee90d96513b691a0f53f394a0e5b8b948ea3b93
Charlie Wilkins <charliewilkins@gmail.com>
2013-10-15 17:19:56 -0700

    add test for user course auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,4 +21,22 @@ describe User do
       user.fullname.should == "#{user.firstname} #{user.lastname}"
     end
   end
+
+  describe 'Course authorization' do
+    it 'is not authorized without a CourseUser object' do
+      user   = create(:user)
+      course = create(:course)
+      expect(user.subscribed_course?(course)).to eq false
+    end
+
+    it 'is authorized when CourseUser object exists' do
+      user               = create(:user)
+      course             = create(:course)
+      course_user        = CourseUser.new
+      course_user.user   = user
+      course_user.course = course
+      course_user.save!
+      expect(user.subscribed_course?(course)).to eq true
+    end
+  end
 end
