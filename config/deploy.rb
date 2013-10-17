ec0fcfd57bc539814de11ef91c744991ebaa76fe
Charlie Wilkins <charliewilkins@gmail.com>
2013-10-17 14:37:47 -0700

    do not throw RTE when course has no owner.

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      execute :bluepill, "restart idealme"
     end
   end
 
@@ -42,6 +43,7 @@ namespace :deploy do
   before "deploy:migrate", "copy_application_yaml"
 
   after :finishing, 'deploy:cleanup'
+
 
 end
 
