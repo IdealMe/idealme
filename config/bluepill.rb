d6393060fabdddcfce12adce02db505c682d3dd0
Charlie Wilkins <charliewilkins@gmail.com>
2013-10-17 15:14:37 -0700

    deploy

--- config/bluepill.rb
+++ config/bluepill.rb
@@ -2,7 +2,7 @@
 
 Bluepill.application("idealme", :log_file => "/home/idealme/apps/idealme/shared/log/bluepill.log") do |app|
   app.process("unicorn") do |process|
-    process.start_command = "/home/idealme/.rvm/bin/idealme_bundle exec unicorn -c config/unicorn/production.rb"
+    process.start_command = "/home/idealme/.rvm/bin/idealme_bundle exec unicorn -c config/unicorn/production.rb -D"
     process.pid_file = "/home/idealme/apps/idealme/shared/tmp/pids/unicorn.pid"
   end
 end
