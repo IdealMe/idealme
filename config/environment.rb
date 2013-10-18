d3b970aa71796f93b284c5a60d176b7aeaa4426a
Charlie Wilkins <charliewilkins@gmail.com>
2013-10-18 06:12:42 -0700

    move smtp settings to production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -12,17 +12,9 @@ Paperclip::Attachment.default_options[:default_url] = '/missing/:class/:attachme
 # Initialize the rails application
 Idealme::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_API_USERNAME'],
-  :password       => ENV['SENDGRID_API_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
 
-unless Rails.env.production?  
+
+unless Rails.env.production?
   #ActiveMerchant::Billing::Base.mode = :test
   #ActiveMerchant::Billing::Base.gateway_mode = :test
   #ActiveMerchant::Billing::Base.integration_mode = :test
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,14 @@ Idealme::Application.configure do
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_API_USERNAME'],
+    :password       => ENV['SENDGRID_API_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
