a9d354a99e58e9fa131839375200211c1369b6d5
Charlie Wilkins <charliewilkins@gmail.com>
2013-10-23 08:14:11 -0700

    recap

--- Capfile
+++ Capfile
@@ -1,27 +1,6 @@
-# Load DSL and Setup Up Stages
-require 'capistrano/setup'
+require 'recap/recipes/rails'
 
-# Includes default deployment tasks
-require 'capistrano/deploy'
+set :application, 'idealme'
+set :repository, 'git@github.com:flingbob/idealme.git'
 
-# Includes tasks from other gems included in your Gemfile
-#
-# For documentation on these, see for example:
-#
-#   https://github.com/capistrano/rvm
-#   https://github.com/capistrano/rbenv
-#   https://github.com/capistrano/chruby
-#   https://github.com/capistrano/bundler
-#   https://github.com/capistrano/rails/tree/master/assets
-#   https://github.com/capistrano/rails/tree/master/migrations
-#
-require 'capistrano/bundler'
-require 'capistrano/rvm'
-require 'capistrano/rails/assets'
-require 'capistrano/rails/migrations'
-#require 'hipchat/capistrano'
-require 'pry'
-
-# Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
-Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
-
+server 'mvp', :app
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,11 +2,8 @@
 set :application, 'idealme'
 set :repo_url, 'git@github.com:flingbob/idealme.git'
 
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :branch, 'origin/mvp'
 
-#set :branch, 'origin/mvp'
-
-set :deploy_to, '~/apps/idealme'
 
 
 # set :scm, :git
@@ -19,7 +16,6 @@ set :deploy_to, '~/apps/idealme'
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-set :keep_releases, 5
 
 
 # hipchat config
@@ -31,30 +27,7 @@ set :hipchat_success_color, 'green' #finished deployment message color
 set :hipchat_failed_color, 'red' #cancelled deployment message color
 set :hipchat_message_format, 'text' # Sets the deployment message format, see https://www.hipchat.com/docs/api/method/rooms/message
 
-SSHKit.config.command_map[:rake] = "~/.rvm/bin/idealme_bundle exec rake"
 
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute("kill -s USR2 $(cat /tmp/pids/puma.idealme.pid)")
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  before "deploy:migrate", "copy_application_yaml"
-  after :finishing, 'deploy:cleanup'
-
-end
 
 
         #require './config/boot'
