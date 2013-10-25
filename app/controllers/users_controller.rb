3e9d26d0cd48ac6635278266741d65f57935fa64
Charlie Wilkins <charliewilkins@gmail.com>
2013-10-25 13:05:26 -0700

    adds chromeless layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def welcome
     flash[:notice] = nil
     @goals = Goal.is_welcome.visible.ordered
+    render layout: "chromeless"
   end
 
   def welcome_save
