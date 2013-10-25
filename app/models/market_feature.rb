78307f4d89ed218c4e044a9c623291da7e0a65ce
Charlie Wilkins <charliewilkins@gmail.com>
2013-10-25 15:37:03 -0700

    market features

--- app/models/market_feature.rb
+++ app/models/market_feature.rb
@@ -2,4 +2,12 @@ class MarketFeature < ActiveRecord::Base
   attr_accessible :description, :avatar, :market_id
 
   belongs_to :market
+
+  has_attached_file :avatar,
+                    :styles => {:thumb => '40x40#', :square => '120x120#'},
+                    :convert_options => {
+                      :thumb => '-gravity center -extent 40x40 -quality 75 -strip',
+                      :square => '-gravity center -extent 120x120 -quality 75 -strip',
+                    }
+
 end
