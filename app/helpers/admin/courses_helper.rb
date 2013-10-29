57853e30209f3451515c926430a1a69f9130c74a
Charlie Wilkins <charliewilkins@gmail.com>
2013-10-29 12:01:56 -0700

    moving course edit page to a tabbed interface

---
+++ app/helpers/admin/courses_helper.rb
@@ -0,0 +1,16 @@
+module Admin::CoursesHelper
+
+  def course_details_active?
+    (params[:active_view] == "details") ? "active" :
+        (params[:active_view].present?) ? "" : "active"
+  end
+
+  def course_previews_active?
+    (params[:active_view] == "previews") ? "active" : ""
+  end
+
+  def course_content_active?
+    (params[:active_view] == "content") ? "active" : ""
+  end
+
+end
