3959f4904428a37acfd16b8164e4f37e2edcf477
Charlie Wilkins <charliewilkins@gmail.com>
2013-10-29 14:05:55 -0700

    undo some debug stuff.

--- app/helpers/admin/courses_helper.rb
+++ app/helpers/admin/courses_helper.rb
@@ -1,14 +1,12 @@
 module Admin::CoursesHelper
 
   def course_details_active?
-    # (params[:active_view] == "details") ? "active" :
-        # (params[:active_view].present?) ? "" : "active"
-    ""
+    (params[:active_view] == "details") ? "active" :
+      (params[:active_view].present?) ? "" : "active"
   end
 
   def course_previews_active?
     (params[:active_view] == "previews") ? "active" : ""
-    "active"
   end
 
   def course_content_active?
