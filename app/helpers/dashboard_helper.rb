1fa61eb4d39220f150b6b57b230d83b3d62515bb
Charlie Wilkins <charliewilkins@gmail.com>
2013-11-13 14:37:55 -0800

    market options

---
+++ app/helpers/dashboard_helper.rb
@@ -0,0 +1,9 @@
+module DashboardHelper
+
+  def market_tags_for_select
+    Market.all.map do |market|
+      ["#{market.name} (#{market.affiliate_tag})", market.affiliate_tag]
+    end
+  end
+
+end
