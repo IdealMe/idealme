c6fb43ddcb677862d49a8029ea07f56693325b7f
Charlie Wilkins <charliewilkins@gmail.com>
2013-11-19 12:54:59 -0800

    enrolled users test

---
+++ spec/features/markets/show_enrolled_spec.rb
@@ -0,0 +1,28 @@
+
+require 'spec_helper'
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe 'market page shows number of enrolled users' do
+  let!(:user)               { create(:user) }
+  let!(:user2)              { create(:user2) }
+  let!(:link)               { create(:affiliate_link) }
+  let!(:affiliate_user)     { link.user }
+  let!(:market)             { create(:market, course: course) }
+  let!(:course)             { create(:course, owner: affiliate_user) }
+
+  before :each do
+    Warden.test_reset!
+  end
+
+  it "show the number of enrolled users", js: true do
+    visit market_path market
+    page.text.should include '0 enrolled'
+    buy_course_as user
+    buy_course_as user2
+    visit market_path market
+    page.text.should include '2 enrolled'
+  end
+end
+
