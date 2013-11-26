0d5eff39e071f24a273f684e863576f14377fda9
Charlie Wilkins <charliewilkins@gmail.com>
2013-11-25 16:01:56 -0800

    fixes account settings form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,6 +19,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #  end
   #end
 
+  def account_update_params
+    params.require(:user).permit!
+  end
+
+  def sign_up_params
+    params.require(:user).permit!
+  end
+
   def after_update_path_for(resource)
     user_path(resource)
   end
