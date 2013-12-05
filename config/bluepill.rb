ef256f11bec8936b306a8a3aecdbcdd9280e9787
Charlie Wilkins <charliewilkins@gmail.com>
2013-12-05 10:57:42 -0800

    blue pill config update - still not working correctly

--- config/bluepill.rb
+++ config/bluepill.rb
@@ -1,8 +1,8 @@
 
 
-Bluepill.application("idealme", log_file: "/home/idealme/apps/idealme/shared/log/bluepill.log") do |app|
-  app.process("unicorn") do |process|
-    process.start_command = "/home/idealme/.rvm/bin/idealme_bundle exec unicorn -c config/unicorn/production.rb -D"
-    process.pid_file = "/home/idealme/apps/idealme/shared/tmp/pids/unicorn.pid"
+Bluepill.application("idealme", log_file: "/var/log/bluepill.log") do |app|
+  app.process("puma") do |process|
+    process.start_command = "/home/idealme/.rvm/bin/idealme_bundle exec puma -c config/puma/production.rb"
+    process.pid_file = "/tmp/pids/puma.idealme.pid"
   end
 end
