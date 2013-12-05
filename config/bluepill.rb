9b8c172123a0e4916dee7e37591f67f7376988f0
Charlie Wilkins <charliewilkins@gmail.com>
2013-12-05 14:36:26 -0800

    blue pill stuff

--- config/bluepill.rb
+++ config/bluepill.rb
@@ -2,7 +2,9 @@
 
 Bluepill.application("idealme", log_file: "/var/log/bluepill.log") do |app|
   app.process("puma") do |process|
-    process.start_command = "/home/idealme/.rvm/bin/idealme_bundle exec puma -c config/puma/production.rb"
-    process.pid_file = "/tmp/pids/puma.idealme.pid"
+    process.start_command    = "bundle exec puma -C config/puma/production.rb"
+    process.pid_file         = "/tmp/pids/puma.idealme.pid"
+    process.start_grace_time = 30.seconds
+    process.daemonize        = true
   end
 end
--- config/puma/production.rb
+++ config/puma/production.rb
@@ -2,7 +2,7 @@
 threads 8,64
 workers 3
 bind 'unix:///tmp/puma.idealme.sock'
-daemonize true
+daemonize false
 pidfile '/tmp/pids/puma.idealme.pid'
 state_path '/tmp/pids/puma.idealme.state'
 stdout_redirect '/home/idealme/app/log/puma.stdout.log', '/home/idealme/app/log/puma.stderr.log', true
