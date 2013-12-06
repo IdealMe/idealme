04c10fece9c24d9144d2fdc280e968d3889d2d62
Charlie Wilkins <charliewilkins@gmail.com>
2013-12-05 17:00:53 -0800

    use pid file

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,7 +3,7 @@ threads 8,64
 workers 3
 bind 'unix:///home/idealme/puma.idealme.sock'
 daemonize false
-#pidfile '/tmp/pids/puma.idealme.pid'
+pidfile '/home/idealme/puma.idealme.pid'
 state_path '/home/idealme/puma.idealme.state'
 stdout_redirect '/home/idealme/app/log/puma.stdout.log', '/home/idealme/app/log/puma.stderr.log', true
 environment 'production'
