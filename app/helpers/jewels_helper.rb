4c3d19ab4eda6c5de176e67a75b7b5a88b1cf552
Charlie Wilkins <charliewilkins@gmail.com>
2013-12-24 08:56:13 -0800

    goal show: active filter is red and not a link

--- app/helpers/jewels_helper.rb
+++ app/helpers/jewels_helper.rb
@@ -2,6 +2,10 @@ module JewelsHelper
   def filter_link(label, filter_name)
     link_class = ''
     link_class = 'active' if params[:filter_name] == filter_name.to_s
-    link_to label, filter_goal_path(filter_name: filter_name), class: link_class
+    if link_class == 'active'
+      content_tag :span, label, class: link_class
+    else
+      link_to label, filter_goal_path(filter_name: filter_name), class: link_class
+    end
   end
 end
