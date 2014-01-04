7f1f85b6c83316926bfc7f06c62646f21cb68735
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-03 18:40:48 -0800

    .

--- db/migrate/20131026003202_change_affiliate_sales.rb
+++ db/migrate/20131026003202_change_affiliate_sales.rb
@@ -3,7 +3,7 @@ class ChangeAffiliateSales < ActiveRecord::Migration
   def change
     rename_column :affiliate_sales, :affiliate_tracking_id, :affiliate_link_id
     #remove_index :affiliate_sales, :affiliate_tracking_id
-    add_index :affiliate_sales, :affiliate_link_id
+    #add_index :affiliate_sales, :affiliate_link_id
   end
 
 end
