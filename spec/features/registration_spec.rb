0120eddabb3bf6043ac384674580e2319507bb70
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-06 12:34:47 -0800

    fix test

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'registration' do
 
-  it 'testing login page', js: true do
+  it 'testing registration', js: true do
     visit new_user_registration_path
     page.should have_content('Create your FREE Ideal Me account')
     fill_in 'user_email', with: 'newguy1000@idealme.com'
@@ -12,7 +12,7 @@ describe 'registration' do
     fill_in 'user_email', with: 'newguy1000@idealme.com'
     fill_in 'user_password', with: 'passpass'
     page.find('input[name="commit"]').click
-    expect(current_path).to eq '/newguy1000/welcome'
+    expect(current_path).to eq '/user/welcome'
   end
 
 end
