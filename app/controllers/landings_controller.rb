ff04ee001a49bd372293e1335809e3fcecdc6fad
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-09 16:33:10 -0800

    workbook

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -4,4 +4,7 @@ class LandingsController < ApplicationController
     @courses = Course.includes(:owner).limit(12)
     @vwo = true
   end
+  def workbook
+    @courses = Course.includes(:owner).limit(12)
+  end
 end
