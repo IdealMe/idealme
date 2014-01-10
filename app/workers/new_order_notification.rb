35e47053cfb5e727b69d38e7998787ce1924f4bb
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-10 12:58:06 -0800

    only in prod

--- app/workers/new_order_notification.rb
+++ app/workers/new_order_notification.rb
@@ -4,6 +4,7 @@ class NewOrderNotification
   sidekiq_options queue: :notifications
 
   def perform(id)
+    return unless Rails.env.production?
     order = Order.where(id: id).first
     if order
       SendHipchatMessage.send("New order: #{order.user.email} - $#{order.course.cost_in_dollars} - #{order.course.name}")
