5aa972b2bf5c4f879de510022dfa7af7ee6a7efb
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-14 11:56:49 -0800

    link up resources page from profile goals

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,6 +4,13 @@ class ResourcesController < ApplicationController
     @articles = Article.where("goal_id IS NOT NULL").all
   end
 
+  def goal
+    @active_tab = :all
+    @goal = Goal.find(params[:goal_id])
+    @articles = Article.where(goal: @goal).all
+    render :index
+  end
+
   def my_goals
     @active_tab = :my_goals
     @articles = Article.where("goal_id IS NOT NULL").all
