03ed3eb66a6d31d846d3af364d2604821901540e
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-21 15:41:11 -0800

    sign up flow stuff

--- spec/features/sign_up_flow_spec.rb
+++ spec/features/sign_up_flow_spec.rb
@@ -27,6 +27,7 @@ describe 'sign up flow with workbook purchase', js: true, vcr: true do
 
   it 'send the user to the create account screen', js: true, vcr: true do
     visit '/getthebook'
+    screenshot
     visit '/aweber_callback?email=charlie%2b22%40idealme%2ecom&from=charlie%2b22%40idealme%2ecom&listname=idealmeoptin&meta_adtracking=idealme%2ecom&meta_message=1&meta_required=email&meta_split_id=&meta_tooltip=&meta_web_form_id=58003487&name=&submit=Submit'
     page.current_path.should eq '/getthebook'
     screenshot
@@ -37,14 +38,21 @@ describe 'sign up flow with workbook purchase', js: true, vcr: true do
 
 
     fill_in "Card Number", with: '4242424242424242'
+    screenshot
     fill_in "Security Code", with: '123'
+    screenshot
     ActiveMerchant::Billing::CreditCard.any_instance.stub(:valid?).and_return(true)
 
     fill_in "First Name", with: "Bean"
+    screenshot
     fill_in "Last Name", with: "Salad"
+    screenshot
     fill_in "Email Address", with: "beansalad@idealme.com"
+    screenshot
     select "01", from: "Card exp month"
+    screenshot
     select "2017", from: "Card exp year"
+    screenshot
     select "Master Card", from: "Card type"
 
     screenshot
@@ -56,13 +64,18 @@ describe 'sign up flow with workbook purchase', js: true, vcr: true do
   end
 
   it 'create account for user that skips workbook order', js: true, vcr: true do
-    visit '/getthebook'
+    visit root_path
+    screenshot
     visit '/aweber_callback?email=charlie%2b22%40idealme%2ecom&from=charlie%2b22%40idealme%2ecom&listname=idealmeoptin&meta_adtracking=idealme%2ecom&meta_message=1&meta_required=email&meta_split_id=&meta_tooltip=&meta_web_form_id=58003487&name=&submit=Submit'
+    screenshot
     page.current_path.should eq '/getthebook'
     find('#skip-btn').click
+    screenshot
     fill_in 'user_password', with: 'passpass'
+    screenshot
     page.find('input[name="commit"]').click
     # goals page
+    screenshot
     page.find('input[name="commit"]').click
     screenshot
     page.current_path.should eq '/resources'
