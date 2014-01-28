cbb3278861152a5ed2843ea9b5d484a07a51bdbc
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-28 15:06:21 -0800

    link to root_path instead of workbook download when user is not logged in

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,9 @@
 module ResourcesHelper
+  def download_url
+    if current_user
+      "http://idealme.s3.amazonaws.com/Design%20Your%20Ideal%20Life.pdf"
+    else
+      "#{root_path}#sign-up"
+    end
+  end
 end
