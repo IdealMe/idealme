71bc82ae53ae615f1af72e1adbad9a68ecaec0f2
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-29 14:33:29 -0800

    remote ip logger

--- app/middleware/remote_ip_logger.rb
+++ app/middleware/remote_ip_logger.rb
@@ -5,7 +5,8 @@ class RemoteIpLogger
 
   def call(env)
     Rails.logger.info env
-    remote_ip = env["HTTP_X_FORWARDED_FOR"]
+    #remote_ip = env["HTTP_X_FORWARDED_FOR"]
+    remote_ip = env["action_dispatch.remote_ip"]
     if remote_ip
       remote_ip = remote_ip.split(',').first
       Rails.logger.info "Remote IP: #{remote_ip}" if remote_ip
