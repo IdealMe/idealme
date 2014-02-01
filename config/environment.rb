29ef0c5fe3b26ab5d771c8fec52db5319a3977cb
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-31 22:28:40 -0800

    cf

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-#Paperclip::Attachment.default_options[:default_url] = '/missing/:class/:attachment/:style.png'
-Paperclip::Attachment.default_options[:default_url] = ActionController::Base.helpers.asset_path('/missing/:class/:attachment/:style.png')
+Paperclip::Attachment.default_options[:default_url] = '/missing/:class/:attachment/:style.png'
+#Paperclip::Attachment.default_options[:default_url] = ActionController::Base.helpers.asset_path('/missing/:class/:attachment/:style.png')
 
 # Initialize the Rails application.
 Idealme::Application.initialize!
