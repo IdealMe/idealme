0bd1978ca3b1dcc45d4a22c130c9bde6a3f25c17
Charlie Wilkins <charliewilkins@gmail.com>
2014-01-31 21:07:14 -0800

    cf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,8 @@ Idealme::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-  #config.action_controller.asset_host = "//dvkd6u0v6m1az.cloudfront.net"
+  #config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "//d1oi23ayjzl9lh.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile += %w( ckeditor/skins/moono/editor.css ckeditor/skins/moono/editor.css ckeditor/lang/en.js )
