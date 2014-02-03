1bb35f5b86d3f7e0e2910178602ed1809882fd0b
Charlie Wilkins <charliewilkins@gmail.com>
2014-02-03 11:54:19 -0800

    asset sync

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -14,7 +14,7 @@ AssetSync.configure do |config|
   # config.fog_region = 'eu-west-1'
   #
   # Don't delete files from the store
-  # config.existing_remote_files = "keep"
+  config.existing_remote_files = "delete"
   #
   # Automatically replace files with their equivalent gzip compressed version
   config.gzip_compression = true
