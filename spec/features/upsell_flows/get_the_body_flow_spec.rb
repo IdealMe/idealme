dbb8fd15c4334d1ee7dbf4d3539f7568031dae06
Charlie Wilkins <charliewilkins@gmail.com>
2014-02-06 14:55:35 -0800

    get body spec

--- spec/features/upsell_flows/get_the_body_flow_spec.rb
+++ spec/features/upsell_flows/get_the_body_flow_spec.rb
@@ -12,24 +12,30 @@ describe 'get the body flow', js: true, vcr: true do
   let!(:course2)            { create(:course2, owner: affiliate_user) }
   let!(:goal)               { create(:goal) }
 
+  before :each do
+    ActiveMerchant::Billing::CreditCard.any_instance.stub(:valid?).and_return(true)
+  end
+
   it 'anonymous user', js: true, vcr: true do
     visit '/get-the-body'
     expect(page).to have_css "form.af-form-wrapper"
 
     visit '/aweber_callback?email=charlie%2b22%40idealme%2ecom&from=charlie%2b22%40idealme%2ecom&listname=idealmeoptin&meta_adtracking=idealme%2ecom&meta_message=1&meta_required=email&meta_split_id=&meta_tooltip=&meta_web_form_id=58003487&name=&submit=Submit'
     expect(page).to_not have_css "form.af-form-wrapper"
-    expect(page.text).to include "Workbook shipped right to your front door for FREE"
+    expect(current_path).to eq '/get-the-body'
 
     fill_in "Card Number", with: '4242424242424242'
     fill_in "Security Code", with: '123'
-    ActiveMerchant::Billing::CreditCard.any_instance.stub(:valid?).and_return(true)
     fill_in "First Name", with: "Bean"
     fill_in "Last Name", with: "Salad"
-    fill_in "Email Address", with: user.email
+
     select "01", from: "Card exp month"
     select "2017", from: "Card exp year"
     select "Master Card", from: "Card type"
     click_button "Complete Purchase"
+
+    expect(page.current_path).to eq '/workbook-thanks'
+    expect(emails.length).to eq 2
     screenshot true
   end
 end
