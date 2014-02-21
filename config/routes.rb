1abceea8f722720ce261450bd54982215759434c
Charlie Wilkins <charliewilkins@gmail.com>
2014-02-21 09:36:10 -0800

    moving buy buttons out of fragment

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Idealme::Application.routes.draw do
   get 'upsell' => 'landings#upsell'
   #get 'continuity-offer' => 'landings#continuity_offer'
   get 'continuity-offer-1' => 'landings#continuity_offer_1'
-  post 'purchase-continuity-offer' => 'landings#purchase_continuity_offer'
+  post 'purchase-continuity-offer' => 'landings#purchase_continuity_offer', as: :purchase_continuity_offer
   get 'continuity-offer-2' => 'landings#continuity_offer_2'
   get 'thanks' => 'landings#thanks'
   get 'thanks/:thanks_type' => 'landings#thanks_custom'
