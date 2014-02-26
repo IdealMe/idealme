f7ae07830f3dc02302bbba3dc07f82cb61c33b29
Charlie Wilkins <charliewilkins@gmail.com>
2014-02-26 06:14:49 -0800

    account for cases where UpdateSubscriptionDays hasn't run for more than 1 day

--- app/workers/update_subscription_days.rb
+++ app/workers/update_subscription_days.rb
@@ -6,15 +6,16 @@ class UpdateSubscriptionDays
 
   def perform
 
-    Rails.logger.info "running update subscription days worker"
-
     Subscription.where("last_update_day_count_at < ? or last_update_day_count_at is null", 1.day.ago).find_each do |sub|
       # how do we validate the subscription is in good standing?
       # for now, assume all subs are valid. We will setup the webhook later
       # TODO
+      sub.last_update_day_count_at = sub.created_at if sub.last_update_day_count_at.nil?
+      gap = Time.now - sub.last_update_day_count_at
+      days = (gap / (60 * 60 * 24)).to_i
 
-      sub.increment :subscribed_days
-      sub.increment :total_days
+      sub.increment :subscribed_days, days
+      sub.increment :total_days, days
       sub.touch :last_update_day_count_at
       sub.save!
 
