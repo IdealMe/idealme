15ee756152d32d8c8ebce04530273cc1e206b536
Charlie Wilkins <charliewilkins@gmail.com>
2014-02-27 17:29:48 -0800

    complete tests for flows

--- spec/features/upsell_flows/phase_one_spec.rb
+++ spec/features/upsell_flows/phase_one_spec.rb
@@ -2,75 +2,26 @@ require 'spec_helper'
 
 describe 'phase one upsell flows' do
 
-  it 'purchase workbook and subscription', js: true, vcr: true do
-    visit '/optin'
-    sleep 0.1
-    visit '/aweber_callback?email=charlie%2b22%40idealme%2ecom&from=charlie%2b22%40idealme%2ecom&listname=idealmeoptin&meta_adtracking=idealme%2ecom&meta_message=1&meta_required=email&meta_split_id=&meta_tooltip=&meta_web_form_id=58003487&name=&submit=Submit'
-    expect(current_path).to eq "/upsell"
+  it 'purchase workbook; purchase subscription', js: true, vcr: true do
+    visit_upsell
     submit_order_form
     expect(current_path).to eq "/continuity-offer-1"
-    page.execute_script("$('#offer-container, #form-container').removeClass('hidden')")
-    sleep 0.1
+    reveal_hidden_elements
     find('#purchase-offer-btn').click
-    sleep 2
-    user = User.order("created_at ASC").last
     expect(user.subscriptions.count).to eq 1
     expect(user.subscriptions.last).to eq Subscription.first
     expect(current_path).to eq "/thanks/thank-you-a"
   end
 
-  it 'purchase workbook and decline everything else', js: true, vcr: true do
-    visit '/optin'
-    sleep 0.1
-    visit '/aweber_callback?email=charlie%2b22%40idealme%2ecom&from=charlie%2b22%40idealme%2ecom&listname=idealmeoptin&meta_adtracking=idealme%2ecom&meta_message=1&meta_required=email&meta_split_id=&meta_tooltip=&meta_web_form_id=58003487&name=&submit=Submit'
-    expect(current_path).to eq "/upsell"
+  it 'purchase workbook; decline subscription; purchase trial subscription', js: true, vcr: true do
+    visit_upsell
     submit_order_form
     expect(current_path).to eq "/continuity-offer-1"
-    page.execute_script("$('#offer-container, #form-container').removeClass('hidden')")
-    sleep 0.1
+    reveal_hidden_elements
     find('#decline-offer-btn').click
     expect(current_path).to eq "/continuity-offer-2"
-    find('.decline-link').click
-    user = User.order("created_at ASC").last
-    expect(user.subscriptions.count).to eq 0
-    expect(current_path).to eq "/thanks/thank-you-d"
-  end
-
-  it 'decline workbook; purchase subscription', js: true, vcr: true do
-    visit '/optin'
-    sleep 0.1
-    visit '/aweber_callback?email=charlie%2b22%40idealme%2ecom&from=charlie%2b22%40idealme%2ecom&listname=idealmeoptin&meta_adtracking=idealme%2ecom&meta_message=1&meta_required=email&meta_split_id=&meta_tooltip=&meta_web_form_id=58003487&name=&submit=Submit'
-    expect(current_path).to eq "/upsell"
-    click_link "No thanks"
-    expect(current_path).to eq "/continuity-offer-1"
-    page.execute_script("$('#offer-container, #form-container').removeClass('hidden')")
-    sleep 1
 
     submit_order_form(email: 'newguy1000@idealme.com')
-    sleep 1
-
-    user = User.where(email: 'newguy1000@idealme.com').order("created_at ASC").last
-    expect(user.subscriptions.count).to eq 1
-    expect(user.subscriptions.last).to eq Subscription.first
-    expect(current_path).to eq "/thanks/thank-you-b"
-  end
-
-  it 'decline workbook; decline continuity-offer-1; purchase trial subscription', js: true, vcr: true do
-    visit '/optin'
-    sleep 0.1
-    visit '/aweber_callback?email=charlie%2b22%40idealme%2ecom&from=charlie%2b22%40idealme%2ecom&listname=idealmeoptin&meta_adtracking=idealme%2ecom&meta_message=1&meta_required=email&meta_split_id=&meta_tooltip=&meta_web_form_id=58003487&name=&submit=Submit'
-    expect(current_path).to eq "/upsell"
-    click_link "No thanks"
-    expect(current_path).to eq "/continuity-offer-1"
-    page.execute_script("$('#offer-container, #form-container').removeClass('hidden')")
-    sleep 1
-
-    click_link "No thanks"
-    expect(current_path).to eq "/continuity-offer-2"
-
-    submit_order_form(email: 'newguy1000@idealme.com')
-
-    user = User.where(email: 'newguy1000@idealme.com').order("created_at ASC").last
     expect(user.subscriptions.count).to eq 1
     expect(user.subscriptions.last).to eq Subscription.first
     expect(user.orders.last.subscription).to eq user.subscriptions.last
@@ -78,24 +29,71 @@ describe 'phase one upsell flows' do
     expect(current_path).to eq "/thanks/thank-you-b"
   end
 
-  it "decline workbook, decline subscription", js: true, vcr: true do
-    visit '/optin'
-    sleep 0.1
-    visit '/aweber_callback?email=charlie%2b22%40idealme%2ecom&from=charlie%2b22%40idealme%2ecom&listname=idealmeoptin&meta_adtracking=idealme%2ecom&meta_message=1&meta_required=email&meta_split_id=&meta_tooltip=&meta_web_form_id=58003487&name=&submit=Submit'
-    expect(current_path).to eq "/upsell"
+  it 'purchase workbook and decline everything else', js: true, vcr: true do
+    visit_upsell
+    submit_order_form
+    expect(current_path).to eq "/continuity-offer-1"
+    reveal_hidden_elements
+    find('#decline-offer-btn').click
+    expect(current_path).to eq "/continuity-offer-2"
+    find('.decline-link').click
+    expect(user.subscriptions.count).to eq 0
+    expect(current_path).to eq "/thanks/thank-you-d"
+  end
 
+  it 'decline workbook; purchase subscription', js: true, vcr: true do
+    visit_upsell
     click_link "No thanks"
     expect(current_path).to eq "/continuity-offer-1"
+    reveal_hidden_elements
 
-    page.execute_script("$('#offer-container, #form-container').removeClass('hidden')")
-    find('.btn-decline-purchase').click
-    expect(current_path).to eq "/continuity-offer-2"
+    submit_order_form(email: 'newguy1000@idealme.com')
 
-    page.execute_script("$('#offer-container').removeClass('hidden')")
-    sleep 0.1
+    expect(user.subscriptions.count).to eq 1
+    expect(user.subscriptions.last).to eq Subscription.first
+    expect(current_path).to eq "/thanks/thank-you-b"
+  end
+
+  it 'decline workbook; decline subscription; purchase trial subscription', js: true, vcr: true do
+    visit_upsell
+    click_link "No thanks"
+    expect(current_path).to eq "/continuity-offer-1"
+    reveal_hidden_elements
 
     click_link "No thanks"
+    expect(current_path).to eq "/continuity-offer-2"
 
+    submit_order_form(email: 'newguy1000@idealme.com')
+
+    expect(user.subscriptions.last).to eq Subscription.first
+    expect(user.orders.last.subscription).to eq user.subscriptions.last
+    expect(user.subscriptions.last.stripe_object["table"]["plan"]["plan"]).to eq "2"
+    expect(current_path).to eq "/thanks/thank-you-b"
+  end
+
+  it "decline workbook; decline subscription; decline trial subscription", js: true, vcr: true do
+    visit_upsell
+    click_link "No thanks"
+    expect(current_path).to eq "/continuity-offer-1"
+    reveal_hidden_elements
+    find('.btn-decline-purchase').click
+    expect(current_path).to eq "/continuity-offer-2"
+    click_link "No thanks"
     expect(current_path).to eq "/thanks/thank-you-c"
+  end
+
+  def reveal_hidden_elements
+    page.execute_script("$('#offer-container, #form-container').removeClass('hidden')")
+  end
+
+  def user
+    User.order("created_at ASC").last
+  end
+
+  def visit_upsell
+    visit '/optin'
+    expect(current_path).to eq "/optin"
+    visit '/aweber_callback?email=charlie%2b22%40idealme%2ecom&from=charlie%2b22%40idealme%2ecom&listname=idealmeoptin&meta_adtracking=idealme%2ecom&meta_message=1&meta_required=email&meta_split_id=&meta_tooltip=&meta_web_form_id=58003487&name=&submit=Submit'
+    expect(current_path).to eq "/upsell"
   end
 end
