7fb9fe89119dbe67de3208eecdf73633883818fc
Charlie Wilkins <charliewilkins@gmail.com>
2014-02-28 17:12:48 -0800

    cke

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,8 +53,8 @@ Idealme::Application.configure do
   config.action_controller.asset_host = "https://d1oi23ayjzl9lh.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  #config.assets.precompile += %w( ckeditor/skins/moono/editor.css ckeditor/skins/moono/editor.css ckeditor/lang/en.js )
-  config.assets.precompile += %w( admin.js )
+  config.assets.precompile += %w( ckeditor/skins/moono/editor.css ckeditor/skins/moono/editor.css ckeditor/lang/en.js )
+  config.assets.precompile += %w( admin.js ckeditor/config.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
