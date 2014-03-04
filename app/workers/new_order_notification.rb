d6c7d0acdb6f3887533ccf48ef9bd50a540e1939
Charlie Wilkins <charliewilkins@gmail.com>
2014-03-04 05:10:25 -0800

    fix new order notification

--- app/workers/new_order_notification.rb
+++ app/workers/new_order_notification.rb
@@ -6,8 +6,12 @@ class NewOrderNotification
   def perform(id)
     return unless Rails.env.production?
     order = Order.where(id: id).first
-    if order
-      SendHipchatMessage.send("New order: #{order.user.email} - $#{order.course.cost_in_dollars} - #{order.course.name}")
+    if order && order.course?
+      SendHipchatMessage.send("New course order: #{order.user.email} - $#{order.course.cost_in_dollars} - #{order.course.name}")
+    elsif order && order.workbook?
+      SendHipchatMessage.send("New workbook order: #{order.user.email}")
+    elsif order && order.subscription?
+      SendHipchatMessage.send("New subscription: #{order.user.email}")
     end
   end
 end
